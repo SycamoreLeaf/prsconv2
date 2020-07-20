731e03d1c2669622ed66841dd870970790024db4
Chris Kimmel <kimmel.95@osu.edu>
2020-07-20 14:14:20 -0400

    Add a test script

---
+++ tests/test1.sh
@@ -0,0 +1,6 @@
+# This script relies on testing data that is not in the shared github repo
+../prsconv2.py --basecalled-subgroup resample_experimet_v2_resquiggle_v2 \
+    --prs-path test1_depth_50k_fisher_3_v14.tombo.per_read_stats \
+    --output-path out1.csv \
+    --chromosome truncated_hiv_rna_genome \
+    --strand "+"
